311fa8d5adf43ae94e41e55bfdd4fb571b6366dd
Duckip <95273591+Duckip@users.noreply.github.com>
2021-12-16 01:53:28 +0800

    Update main.c

--- main.c
+++ main.c
@@ -22,7 +22,62 @@ int search();
 int sort_nilai();
 int sort_nama();
 
+void loading()
+{
+    printf("\n\n\n\n\n\n\n\t\t\t\t\t\t");
+    printf("\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...\n\n");
+    printf("\t\t\t\t\t\t");
+
+    // Print loading bar progress
+    for (int i = 0; i < 20; i++) {
+        printf("%c", 9); // kalau mau simbol lain tinggal ubah angkanya
+        Sleep(100);
+    }
+}
+
 int main(){
-    printf("\nHello Word\n");
+    int pil, ul;
+    
+    loading();
+    do{
+    system("cls");
+    menu:
+    printf("\n  Program Input Nilai");
+    printf("\n  ---------------------");
+    printf("\n  1. Input Data");
+    printf("\n  2. Edit Data");
+    printf("\n  3. Display Data");
+    printf("\n  4. Keluar");
+    printf("\n  ---------------------");
+    printf ("\n Ketik pilihan Anda: ");
+    scanf  ("%d", &pil);
+    system("cls");
+    switch (pil)
+    {
+        case 1:
+            //input
+            break;
+
+        case 2:
+            //edit
+            break;
+            
+        case 3:
+            //lihat
+            break;
+
+        case 4:
+            return 0;
+
+        default:
+            printf("\n\n\t Maaf, pilihan tidak tersedia ");
+            fflush (stdin);
+            goto menu;
+            break;
+    }
+        fflush (stdin);
+        printf("\n\n Apakah anda ingin mengulang? Ketik 1 jika iya: ");
+        scanf("%d", &ul);
+    }while(ul=='1');
     return 0;
 }
