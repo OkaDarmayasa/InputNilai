f46cfebc097b5235bb94a84ca16e356d17638842
OkaDarmayasa <79191618+OkaDarmayasa@users.noreply.github.com>
2021-12-16 13:09:11 +0800

    Ngubah isi dari file utama (main.c)
    Jadi, di file main ini akan berpatokan dengan file Input.c. 
    Berpatokannya di bagian variabel (structnya aku pindahin ke file Input.c). Sehingga, pada file main.c ini tidak terdapat variabel struct penyimpan data mahasiswa.

--- main.c
+++ main.c
@@ -1,21 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 100
-
-//Struktur data yang akan menyimpan data mahasiswa
-struct data 
-{
-    char NIM[10];
-    char Nama[30];
-    int Nilai[6]; 
-    //indeks 0 tugas, 1 quiz, 2 keaktifan, 3 UTS, 4 UAS, 5 rata-rata.
-};
-
-struct data mhs[MAX];
+#include "Input.c"
 
 //Deklarasi fungsi
 void landing_page();
-int input_data();
 void edit_data();
 void display();
 int search();
@@ -37,47 +25,46 @@ void loading()
 
 int main(){
     int pil, ul;
-    
+
     loading();
     do{
-    system("cls");
-    menu:
-    printf("\n  Program Input Nilai");
-    printf("\n  ---------------------");
-    printf("\n  1. Input Data");
-    printf("\n  2. Edit Data");
-    printf("\n  3. Display Data");
-    printf("\n  4. Keluar");
-    printf("\n  ---------------------");
-    printf ("\n Ketik pilihan Anda: ");
-    scanf  ("%d", &pil);
-    system("cls");
-    switch (pil)
-    {
-        case 1:
-            //input
-            break;
+        system("cls");
+        menu:
+        printf("\n  Program Input Nilai");
+        printf("\n  ---------------------");
+        printf("\n  1. Input Data");
+        printf("\n  2. Edit Data");
+        printf("\n  3. Display Data");
+        printf("\n  4. Keluar");
+        printf("\n  ---------------------");
+        printf ("\n Ketik pilihan Anda: ");
 
-        case 2:
-            //edit
-            break;
-            
-        case 3:
-            //lihat
-            break;
+        scanf  ("%d", &pil);
+        system("cls");
 
-        case 4:
-            return 0;
-
-        default:
-            printf("\n   Maaf, pilihan tidak tersedia\n");
-            fflush (stdin);
-            goto menu;
-            break;
-    }
+        switch (pil)
+        {
+            case 1:
+                input();
+                break;
+            case 2:
+                //edit
+                break;
+            case 3:
+                //lihat
+                break;
+            case 4:
+                return 0;
+            default:
+                printf("\n   Maaf, pilihan tidak tersedia\n");
+                fflush (stdin);
+                goto menu;
+                break;
+        }
         fflush (stdin);
         printf("\n\n Apakah anda ingin mengulang? Ketik 1 jika iya: ");
         scanf("%d", &ul);
     }while(ul==1);
+
     return 0;
 }
