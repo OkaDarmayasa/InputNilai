1b2381763170a2eec90063fb3f07e754c4029e9a
OkaDarmayasa <79191618+OkaDarmayasa@users.noreply.github.com>
2021-12-16 16:33:05 +0800

    Fungsi edit untuk mengedit nilai sesuai NIM
    Jadi akan disearch terlebih dahulu pemilik data (NIMnya) apakah datanya sudah pernah dimasukkan atau belom. 
    Kalau sudah, maka bisa diedit. Cara ngeditnya sama ae kayak input karena tinggal meng-assign nilai baru ke dalam struct.
    Kalau belom, ya ga bisa ngedit. Program akan kembali ke menu edit.

---
+++ Edit.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "Display.c"
+
+void edit(){
+    char NIMEdit[11], ulang;
+    int pilihan;
+    printf("\n  NIM: ");
+    scanf("\n");
+    scanf("%[^\n]*c", NIMEdit);
+
+    //mencari NIM yang ingin diedit di struct, apakah sudah ada atau belom
+    int indeksEdit = search(NIMEdit);
+    if (indeksEdit != -1){ //berarti ada
+        ulang:
+        printf("\n  Menu Input Data Mahasiswa");
+        //                 2008561070
+        printf("\n         %s", mhs[indeksEdit].NIM);
+        printf("\n  ---------------------");
+        printf("\n  1. Edit Nilai Tugas");
+        printf("\n  2. Edit Nilai Quiz");
+        printf("\n  3. Edit Nilai Keaktifan");
+        printf("\n  4. Edit Nilai UTS");
+        printf("\n  5. Edit Nilai UAS");
+        printf("\n  ---------------------");
+        printf ("\n Ketik pilihan Anda: ");
+        scanf("%d", &pilihan);
+
+        // Pengecekan klo yang dipilih enggak ada di menu
+        if (pilihan > 5 || pilihan < 1){
+            printf("    Pilihan Tidak Valid!\n");
+            goto ulang;
+        }
+
+        printf("\n  Nilai    : ");
+        scanf("%d", &mhs[indeksEdit].Nilai[pilihan-1]);
+
+        //Menghitung nilai rata-rata
+        int rerata = 0;
+        for (int i = 0; i < 5; i++){
+            rerata += mhs[indeksEdit].Nilai[i];
+        }
+        mhs[indeksEdit].Nilai[5] = rerata/5;
+
+        printf(" Ingin mengedit data lagi? Ketik 1 jika iya: ");
+        scanf("\n");
+        scanf("%c", &ulang);
+        if (ulang == '1'){
+            goto ulang;
+        }
+    }else {//berarti enggak ada
+        printf("    NIM Tidak Ada\n");
+        printf("    Masukkan NIM Kembali\n");
+        edit();
+    }
+}
+
+
